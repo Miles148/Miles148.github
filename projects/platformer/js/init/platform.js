15ff01398512fb31bd8c2cbb2ebddd0b417917e9
Miles148 <Milesclute@icloud.com>
2023-01-10 19:20:19 +0000

    js game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -38,7 +38,8 @@
 
     // example:
     createPlatform(400, 250);
-    createPlatform(170, 460, 0.5);
+    createPlatform(170, 500, 0.5);
+    createPlatform(450, 590, 0.5);
 
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
